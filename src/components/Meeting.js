3aa083d1901cdfd29b032c61a1e4bf9dd0b09928
a1528zhang <beingszhang@gmail.com>
2017-01-21 22:59:05 +0800

    agro

---
+++ src/components/Meeting.js
@@ -0,0 +1,42 @@
+import React, { Component, PropTypes } from 'react'
+import { render } from 'react-dom'
+import AgoraRTC from './AgoraRTCSDK-1.8.0'
+
+class Meeting extends Component {
+    render() {
+        let channel = "2";
+        let client = AgoraRTC.createRtcClient();
+        console.log(AgoraRTC.createClient());
+        client.join(channel, undefined, function(uid) {
+            console.log("User " + "b7c2835c0fc941e480664d982f9dd88a" + " join channel successfully");
+            console.log("Timestamp: " + Date.now());
+            localStream = AgoraRTC.createStream({
+                streamID: "b7c2835c0fc941e480664d982f9dd88a",
+                audio: true,
+                cameraId: videoSource.value,
+                microphoneId: "",
+                video: true,
+                screen: false
+            });
+            localStream.setVideoProfile("120P");
+            localStream.init(function(){
+                displayStream('66666', localStream,"100", "100", '');
+            })
+        });
+        return (
+            <div>
+                <p>meeting</p>
+                <input placeholder="roomNum"></input>
+                <div id="66666">
+                    <div id="66666b7c2835c0fc941e480664d982f9dd88a" class="video-item" data-stream-id="b7c2835c0fc941e480664d982f9dd88a" style="width:100px;height:100px"></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+function displayStream(tagId, stream, width, height, className, parentNodeId) {
+    stream.play("66666b7c2835c0fc941e480664d982f9dd88a");
+}
+
+export default Meeting;
--- src/router.js
+++ src/router.js
@@ -3,14 +3,13 @@ import { Router, Route, Link,hashHistory } from 'react-router';
 import { render } from 'react-dom'
 import App from './containers/app'
 import World from './containers/appWorld'
+import Meeting from './components/Meeting'
 
 class Routers extends Component {
     render(){
         return (
             <Router history={hashHistory}>
-                <Route path="/" component={App}>
-                    <Route path="/world" component={World}>
-                    </Route>
+                <Route path="/" component={Meeting}>
                 </Route>
             </Router>
         )
